061931ad55bb1a196485484ed4055c3f55361038
Hugo Aparecido Gregorio de Almeida <hugoapga626@gmail.com>
2025-10-23 11:12:54 -0300

    Update Vetor.cpp

--- Aula15/Vetor.cpp
+++ Aula15/Vetor.cpp
@@ -2,26 +2,31 @@
 
 using namespace std;
 
-class Array{
-    private:
-        int *ptr;
-        int size;
-    public:
+class Array
+{
+private:
+    int *ptr;
+    int size;
+
+public:
     Array(int = 10);
-    Array (const Array&);
+    Array(const Array &);
     ~Array();
     int getSize();
+    int &operator[](int);
+    const int operator[](int) const;
 };
 
-Array::Array(int size){
-
+Array::Array(int size)
+{
 }
 
-Array::Array(const Array &){
-
+Array::Array(const Array &)
+{
 }
 
-int main(){
+int main()
+{
 
     return 0;
 }
